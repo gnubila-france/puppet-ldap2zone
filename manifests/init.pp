4f0e0a9453bb1b15967f2b89eed25532f924af5f
Baptiste Grenier <bgrenier@gnubila.fr>
2014-07-09 17:49:27 +0200

    ldap2zone: use anchor pattern to fix dependency order.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 class ldap2zone (
-  $ldap_server_uri = params_lookup('ldap_server_uri')
+  $ldap_server_uri = $ldap2zone::params::ldap_server_uri,
   ) inherits ldap2zone::params {
-  include ldap2zone::install
-  include ldap2zone::config
+  anchor { 'ldap2zone::begin': } ->
+  class { 'ldap2zone::install': } ->
+  class { 'ldap2zone::config': } ->
+  anchor { 'ldap2zone::end': }
 }
 
 # vim: set et sta sw=2 ts=2 softtabstop=2 noci noai:
