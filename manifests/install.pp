c7ac655985b6802f73645ea2fe5682ed6eac8a1b
Baptiste Grenier <bgrenier@gnubila.fr>
2014-07-10 16:44:16 +0200

    install: Install ldap-utils.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,9 @@ class ldap2zone::install {
   package { 'ldap2zone':
     ensure => 'latest',
   }
+  package { 'ldap-utils':
+    ensure => 'latest',
+  }
 }
 
 # vim: set et sta sw=2 ts=2 softtabstop=2 noci noai:
